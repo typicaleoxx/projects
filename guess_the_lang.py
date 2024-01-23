e5863b1b4bd7e9047496da8f185af63480b3b2cf
typicaleoxx <lsneha991@gmail.com>
2024-01-23 22:10:04 +0545

    initial commit

---
+++ guess_the_lang.py
@@ -0,0 +1,58 @@
+import random
+emoji_languages = {
+    "🐍": "python",
+    "🌐": "javascript",
+    "☕": "java",
+    "💻": "c++",
+    "💎": "ruby",
+    "📄": "html",
+    "🎨": "css",
+    "🐘": "php",
+    "🍏": "swift",
+    "🔒": "c#",
+    "🔍💼": "typescript",
+    "🚀": "go",
+    "🦀": "rust",
+    "🔥": "kotlin",
+    "🐪": "perl",
+}
+
+def main():
+    user_ready=input("Are you ready for a fun round? (yes/no)").lower()
+    try:
+        if user_ready=="yes" or user_ready=="no":
+            if user_ready=="yes":
+                game()
+            else:
+                print("Okay, have a great time ahead.")
+    except:
+        print("Invalid choice. Try again")
+        
+
+def game():
+    score=0
+    for i in range(5):
+        list_emoji_languages=list(emoji_languages.keys())
+        game_guess=random.choice(list_emoji_languages)
+        user_guess=input(f"Guess language({game_guess}): ").lower()
+        if emoji_languages.get(game_guess)==user_guess:
+            print("Congratulations. You guessed it right.")
+            score+=1
+        else:
+            print(f"Oops, wrong guess. It's {emoji_languages.get(game_guess)}.")
+    total_score(score)
+
+def total_score(score):
+    print(f"Congratulations! You scored {score} out of 5")
+    ask=input("Do you want to play again? (yes/no)").lower()
+    try:
+        if ask=="yes" or ask=="no":
+            if ask=="yes":
+                game()
+            elif ask=="no":
+                print("It was nice seeing you. Have a great day ahead.")
+    except:
+        print("Oops, wrong input. Try again.")
+        
+main()
+        
