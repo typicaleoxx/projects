e7b67d2b2ce2d54c5fe2992a3be2ce8a7ab39cae
typicaleoxx <lsneha991@gmail.com>
2024-02-24 21:45:13 +0545

    management urls update

--- HotelManagementSystem/management/urls.py
+++ HotelManagementSystem/management/urls.py
@@ -10,6 +10,11 @@ urlpatterns = [
                 "post": "create",
             }
         ),
+    ),
+    # url ko difference anusar diff path rakheko, kina ki to retrieve specific data, or delete specific, we have to send id of specific data
+    path(
+        "room-type/<int:pk>/",
+        RoomTypeView.as_view({"get": "retrieve", "put": "update", "delete": "destroy"}),
     ),  # class based views use garda as_view() method call garnu parcha
 ]  # in crud create garna post request
 # retrieve garna get,update-put. delete-delete
