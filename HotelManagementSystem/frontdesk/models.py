081b5987398cdd6e5c4b70ede720f68447268c9d
typicaleoxx <lsneha991@gmail.com>
2024-02-24 18:11:25 +0545

    defined frontdesk models

--- HotelManagementSystem/frontdesk/models.py
+++ HotelManagementSystem/frontdesk/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from management import Room
+
 
 # Create your models here.
+class Customer(models.Model):
+    name = models.CharField(max_length=200)
+    address = models.CharField(max_length=200)
+    email = models.EmailField()
+    contact_no = models.IntegerField()
+
+
+# hamle table create gareko bhayera if customer comes again, feri define garirakhnu parena
+REQUIRED_FIELD = ["email"]
+
+
+class CustomerRoom(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True)
+    room = models.ForeignKey(Room, on_delete=models.SET_NULL, null=True)
