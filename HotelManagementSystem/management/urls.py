a90d5f2743d09266ef48750c5dd24f23665e604d
typicaleoxx <lsneha991@gmail.com>
2024-02-24 21:14:24 +0545

    created sep management urls

---
+++ HotelManagementSystem/management/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import RoomTypeView
+
+urlpatterns = [
+    path("room-type/", RoomTypeView.as_view({"get":"list","post":"create",})),#class based views use garda as_view() method call garnu parcha
+]#incrud create garna post request
+#retrieve garna get,update-put. delete-delete
+    
