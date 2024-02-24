3486c62f2b7d474934de81492e497344533c2b2a
typicaleoxx <lsneha991@gmail.com>
2024-02-24 21:38:45 +0545

    update management urls

--- HotelManagementSystem/management/urls.py
+++ HotelManagementSystem/management/urls.py
@@ -2,7 +2,14 @@ from django.urls import path
 from .views import RoomTypeView
 
 urlpatterns = [
-    path("room-type/", RoomTypeView.as_view({"get":"list","post":"create",})),#class based views use garda as_view() method call garnu parcha
-]#incrud create garna post request
-#retrieve garna get,update-put. delete-delete
-    
+    path(
+        "room-type/",
+        RoomTypeView.as_view(
+            {
+                "get": "list",
+                "post": "create",
+            }
+        ),
+    ),  # class based views use garda as_view() method call garnu parcha
+]  # in crud create garna post request
+# retrieve garna get,update-put. delete-delete
