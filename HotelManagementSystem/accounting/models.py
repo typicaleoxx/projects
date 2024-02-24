f7ac82991bcdba94c319e9b7dbdeb436613988a8
typicaleoxx <lsneha991@gmail.com>
2024-02-24 18:16:55 +0545

    created accountingapps models

--- HotelManagementSystem/accounting/models.py
+++ HotelManagementSystem/accounting/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from frontdesk import Customer
+
 
 # Create your models here.
+class Bill(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True)
+    total_amount = models.IntegerField()
+
+
+class Payment(models.model):
+    bill = models.OneToOneField(Bill, on_delete=models.SET_NULL, null=True)
+
+
+# euta bill lai ekchoti matra pay garne bhayera onetoone relationship bhako
+# so tyo relationship use garna onetoonefield user gareko
